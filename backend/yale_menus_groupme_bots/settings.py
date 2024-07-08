e3621a0ff70d84edcf1b8a0f5e5a96a42bddda20
Chase Reynders <chase.reynders@yale.edu>
2024-07-08 01:06:00 -0400

    postgresql update

--- backend/yale_menus_groupme_bots/settings.py
+++ backend/yale_menus_groupme_bots/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # load dotenv
 load_dotenv()
@@ -80,12 +81,18 @@ WSGI_APPLICATION = 'yale_menus_groupme_bots.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': dj_database_url.config(
+         default=os.environ.get('DATABASE_URL')
+     )
+ }
 
 
 # Password validation
