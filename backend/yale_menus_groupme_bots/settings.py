f002beca6fad3e7b112b98baab6ab4d897697b30
Ubuntu <ubuntu@ip-172-31-26-92.us-east-2.compute.internal>
2024-07-09 18:17:09 +0000

    static ignored

--- backend/yale_menus_groupme_bots/settings.py
+++ backend/yale_menus_groupme_bots/settings.py
@@ -50,11 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'prefs',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
